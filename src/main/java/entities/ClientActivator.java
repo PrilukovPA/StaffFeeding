110f0858e7c8d507c1d79b4020b23f303f6d8df6
intro <contact84@yandex.ru>
2019-05-24 22:59:54 +0500

    add

--- src/main/java/entities/ClientActivator.java
+++ src/main/java/entities/ClientActivator.java
@@ -1,4 +1,13 @@
 package entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ClientActivator {
+
+    @JsonProperty("is_activated_by_distributor")
+    private final boolean isActivatedByDistributor;
+
+    public ClientActivator(boolean isActivatedByDistributor) {
+        this.isActivatedByDistributor = isActivatedByDistributor;
+    }
 }
